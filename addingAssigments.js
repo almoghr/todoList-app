7a43f721b0082d922729f4d235900f552fb9e12a
almog <almoghr36@gmail.com>
2020-03-24 14:44:05 +0200

    added assigments on enter key, moving to toggle colors

---
+++ addingAssigments.js
@@ -0,0 +1,26 @@
+const todos = [];
+const TDinp = document.querySelector('.todo-input');
+const doneTODOS = []
+const body = document.querySelector('body');
+const list = document.querySelector('.todo-list');
+
+
+body.addEventListener('keyup', (event) => {
+    if (TDinp.value && event.keyCode === 13){
+        const item = {
+            id: Date.now(),
+            content: TDinp.value,
+        }
+        todos.push(item)
+        TDinp.value = '';
+        list.appendChild(createTodoItem(item))
+    }
+});
+function createTodoItem(item) {
+    const todo = document.createElement('div');
+    todo.classList.add('todo-item')
+    todo.innerHTML =`<span>${item.content}</span>
+    <button class="todo-remove" todo-id="${item.id}">REMOVE</button>
+    <button class="todo-done" todo-id="${item.id}">DONE</button>`
+    return todo
+}
